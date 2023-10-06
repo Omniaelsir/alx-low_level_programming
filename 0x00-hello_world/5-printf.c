4adf8be74532460706b0f5c0251a438e76fbc2d3
omniaelsir <omniatagelsir1@gmail.com>
2023-10-05 18:08:09 -0700

    commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: returns 0 on success
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
