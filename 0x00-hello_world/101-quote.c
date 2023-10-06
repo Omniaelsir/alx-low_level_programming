30d8a8ea5c372c9db0610643a9a0881245e68ab5
omniaelsir <omniatagelsir1@gmail.com>
2023-10-05 18:27:51 -0700

    commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: returs 1 on success
+ */
+
+int main(void)
+
+{
+	char c[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	putchar(c);
+
+	return (1);
+}
