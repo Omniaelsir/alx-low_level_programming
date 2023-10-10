8dcdd6ffba596d06a13b1c2317b5339798f88278
omniaelsir <omniatagelsir1@gmail.com>
2023-10-10 15:00:11 -0700

    “commit”

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -19,10 +19,10 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	if (n > 0)
-		printf("%d is postive", &n);
-	if (n == 0)
-		printf("%d is Zero", &n);
-	else if (n < 0)
-		printf("%d is negative", &n);
+		printf("%d is positive", n);
+	else if (n == 0)
+		printf("%d is Zero", n);
+	else
+		printf("%d is negative", n);
 	return (0);
 }
