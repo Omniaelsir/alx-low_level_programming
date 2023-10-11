5f0c424215eb074ad206dd72cac862c932dfdcf8
omniaelsir <omniatagelsir1@gmail.com>
2023-10-11 13:29:34 -0700

    “commit”

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:  prints all single digit numbers of base 10
+ *
+ * Return: returns (0) on success
+*/
+
+int main(void)
+{
+	int x = 0;
+
+	while (x < 10)
+		printf("%i\n", x);
+		x++
+	return (0);
+}
