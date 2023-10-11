1fc8a34617ddb629241fe6d2a0dd348f83a3035f
omniaelsir <omniatagelsir1@gmail.com>
2023-10-11 16:15:51 -0700

    “commit”

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Desciption:  prints all possible combinations of single-digit numbers
+ *
+ * Return: returns (0) on success
+*/
+
+int main(void)
+{
+	int x = 0;
+
+	while (x <= 8)
+	{
+		putchar(x);
+		putchar (', ');
+	}
+	putchar(9);
+	return (0);
+}
