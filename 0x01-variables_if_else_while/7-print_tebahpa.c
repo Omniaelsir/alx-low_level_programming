5ce4e14e096bcc832e35e5b0c670fde344bee44c
omniaelsir <omniatagelsir1@gmail.com>
2023-10-11 15:31:47 -0700

    “commit”

---
+++ 0x01-variables_if_else_while/7-print_tebahpa.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Description: print alphabet in reverse
+ *
+ * Return: returns (0) on success
+*/
+
+int main(void)
+{
+	int x = 122;
+
+	while (x >= 97)
+	{
+		putchar(x);
+		x--;
+	}
+	putchar('\n');
+}
