6ee82e20ab369627abd7bfef0b48e7533d51bc66
omniaelsir <omniatagelsir1@gmail.com>
2023-10-11 17:00:49 -0700

    “commit”

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all possible unique combinations 
+ * 		of two digits
+ *
+ * Return: returns (0) on success*/
+
+int main(void)
+{
+	int x = 48;
+	int b = 48;
+
+	while (x <= 57)
+	{
+		while (b <= 57)
+		{
+			if (x != b && x < b)
+			{
+				putchar(x);
+				putchar(b);
+
+				if (x + b != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			b++;
+		}
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
