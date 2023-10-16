4c75aea0bff0db679cdd7b374d63c4d15c12a9ce
omniaelsir <omniatagelsir1@gmail.com>
2023-10-16 04:03:14 -0700

    “commit”

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_last_digit - function prints last digit
+ *
+ * @x: function array of parameters
+ *
+ * Return: returns 0 on success
+*/
+
+int print_last_digit(int x[])
+{
+	int y = x[-1];
+
+	_putchar(y);
+	return (0);
+}
