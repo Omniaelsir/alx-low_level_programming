ab92301022ae11a2ecc852aed57469298b505483
omniaelsir <omniatagelsir1@gmail.com>
2023-10-16 18:53:39 -0700

    “0x05-pointers_arrays_strings”

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - function reverse string
+ *
+ * @s: function parameter
+ *
+ * Return: nothing to return
+*/
+
+void rev_string(char *s)
+{
+	int length;
+	int x;
+	int swap;
+
+	for (length = 0; *s != '\0'; ++length)
+		;
+	for (x = 0; x < length / 2; x++)
+	{
+		swap = s[x];
+		s[x] = s[length - 1 - x];
+		s[length - 1 - x] = swap;
+	}
+
