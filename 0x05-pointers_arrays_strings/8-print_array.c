860718e38e941b0d9a8b27bd6389fc7a4530b9a3
omniaelsir <omniatagelsir1@gmail.com>
2023-10-16 19:34:38 -0700

    “0x05-pointers_arrays_strings”

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_array - prints array of integers
+ *
+ * @a: function parameter
+ *
+ * @n: function parameter
+ *
+ * Return: void
+*/
+
+void print_array(int *a, int n)
+{
+	int x;
+
+	for (x = 0; x < n; x++)
+	{
+		if (x != (n - 1))
+			print("%d, ", a[x]);
+		else
+			printf("%d", a[x]);
+	}
+	printf("\n");
+
+}
