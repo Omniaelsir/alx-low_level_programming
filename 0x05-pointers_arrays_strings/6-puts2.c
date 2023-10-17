fdbc0fc4ba01ce680fb19a15e2ab7b8dd835034b
omniaelsir <omniatagelsir1@gmail.com>
2023-10-16 19:12:09 -0700

    “0x05-pointers_arrays_strings”

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - prints string starting with the first character
+ *
+ * @str: function parameter
+ *
+ * Return: void
+*/
+
+void puts2(char *str)
+{
+	int x;
+
+	for (x = 0; str[x] != '\0''; ++x)
+	{
+		if (x % 2 == 0)
+			_putchar(str[x]);
+	}
+	_putchar('\n');
+}
