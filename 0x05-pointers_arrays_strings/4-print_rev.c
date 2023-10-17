7a64de51566b87f1687734f1835f6aac2b5a1f5f
omniaelsir <omniatagelsir1@gmail.com>
2023-10-16 18:38:54 -0700

    “0x05-pointers_arrays_strings”

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,23 @@
 #include "main.h"
 
-/***/
+/**
+ * print_rev - prints string in reverse
+ *
+ * @n: function parameter
+ *
+ * Return: no return
+*/
 
+void print_rev(char *s)
+{
+	int n;
 
+	while (s[n] != '\0')
+		n++;
+
+	while (n--)
+	{
+		_putchar(s[n]);	
+	}
+	_putchar('\n');
+}
