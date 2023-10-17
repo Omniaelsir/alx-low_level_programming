49c6f6029d8863271f48152548b70db43e48a2e3
omniaelsir <omniatagelsir1@gmail.com>
2023-10-16 19:22:34 -0700

    “0x05-pointers_arrays_strings”

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of string
+ *
+ * @str: function parameter
+ *
+ * Return: void
+*/
+
+void puts_half(char *str)
+{
+	int x;
+
+	for (x = 0; str[x] != '\0'; x++)
+		;
+	x++
+	for (x /= 2; str[x] != '\0'; x++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
