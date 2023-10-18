211082ac90977dfb87ddfcba406df76ecceb76ce
omniaelsir <omniatagelsir1@gmail.com>
2023-10-17 18:18:07 -0700

    0x02-function_nested_loops

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -18,7 +18,7 @@ void jack_bauer(void)
 			_putchar((x % 10) + 48);
 			_putchar(':');
 			_putchar((y / 10) + 48);
-			_putchar((n % 10) + 48);
+			_putchar((y % 10) + 48);
 			_putchar('\n');
 		}
 	}
