c8b694b8cb74aad03185baf02e0241e9b094f16e
omniaelsir <omniatagelsir1@gmail.com>
2023-10-17 18:39:59 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table
+ *
+ * Return: void
+*/
+
+void times_table(void)
+{
+	int x, y, sum;
+	sum = x * y;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+			if ( y != 9)
+			{
+				_putchar(sum + 48);
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+				_putchar(sum + 48);
+		}
+	}
+}
