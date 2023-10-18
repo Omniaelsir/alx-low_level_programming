aaf564b2aec03176e81985c2aed5db0c44e32a24
omniaelsir <omniatagelsir1@gmail.com>
2023-10-17 18:17:16 -0700

    0x02-function_nested_loops

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,24 +8,18 @@
 
 void jack_bauer(void)
 {
-	int x, y, z, n;
+	int x, y;
 
 	for (x = 0; x <= 2; x++)
 	{
-		for (y = 0; y <= 3; y++)
+		for (y = 0; y <= 9; y++)
 		{
-			for (z = 0; z <= 5; z++)
-			{
-				for (n = 0; n <= 9; n++)
-				{
-					_putchar(x + 48);
-					_putchar(y + 48);
-					_putchar(':');
-					_putchar(z + 48);
-					_putchar(n + 48);
-					_putchar('\n');
-				}
-			}
+			_putchar((x / 10) + 48);
+			_putchar((x % 10) + 48);
+			_putchar(':');
+			_putchar((y / 10) + 48);
+			_putchar((n % 10) + 48);
+			_putchar('\n');
 		}
 	}
 }
