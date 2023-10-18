510737f9efbe062d8d59c80b7559b2ce4c5e5032
omniaelsir <omniatagelsir1@gmail.com>
2023-10-18 11:18:19 -0700

    0x02-nested_loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -25,16 +25,16 @@ void print_times_table(int n)
 				if (mult > 99)
 				{	_putchar((mult / 100) + 48);
 					_putchar((mult / 10) % 10 + 48);
-					_putchar((mult % 10) + 48);}
+					_putchar((mult % 10) + 48); }
 				else if (mult > 9 && mult <= 99)
 				{	_putchar(' ');
 					_putchar((mult / 10) + 48);
-					_putchar((mult % 10) + 48);}
+					_putchar((mult % 10) + 48); }
 				else
 				{	_putchar(' ');
 					_putchar(' ');
-					_putchar(mult + 48);}}
-			_putchar('\n');}}
+					_putchar(mult + 48); }}
+			_putchar('\n'); }}
 	if (n == 0)
 	{	_putchar('0');
-		_putchar('\n');}}
+		_putchar('\n'); }}
