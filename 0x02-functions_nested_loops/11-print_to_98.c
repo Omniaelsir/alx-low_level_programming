6b7c7b176d62ac71928ed6ea0e7ccac5db36a2b1
omniaelsir <omniatagelsir1@gmail.com>
2023-10-18 08:28:24 -0700

    0x02-nested_loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,22 +5,23 @@
  * print_to_98 - function prints numbers to 98
  *
  * @n: function parameter
- *
- * Return: returns 0 on success
 */
 
 void print_to_98(int n)
 {
 	if( n <= 98)
 	{
-		for (int x = n; x <= 98; x++)
+		int x;
+
+		for (x = n; x <= 98; x++)
 			printf("%d", x);
 
 	}
 	else
 	{
-		for (int y = n; y >= 98; y--)
+		int y;
+
+		for (y = n; y >= 98; y--)
 			printf("%d", y);
 	}
-	return(0);
 }
