27d2941dae065834beb5f79e155c06706482f420
omniaelsir <omniatagelsir1@gmail.com>
2023-10-18 07:54:44 -0700

    0x02-nested_loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * add - function adds two integers
+ *
+ * @x: function parameter
+ *
+ * @y: function parameter
+*/
+
+int add(int x, int y)
+{
+	int sum = x + y;
+
+	if (sum < 10)
+		_putchar(sum + 48);
+	else
+	{
+		_putchar((sum / 10) + 48);
+		_putchar((sum % 10) + 48);
+	}
+	return (0);
+}
