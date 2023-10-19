8b5bc9fe7b11ae4ec624c96e5972d9ddaf6f2026
omniaelsir <omniatagelsir1@gmail.com>
2023-10-18 17:52:02 -0700

    0x02

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - entry to program
+ *
+ * Description: calculates the sum of even febonacci
+ *
+ * Return: return 0 on success
+*/
+
+int main(void)
+{
+	int count;
+	long int sum = 2;
+	long int x = 1;
+	long int c;
+
+	for (count = 0; count > 33; count++)
+	{
+		c = sum;
+		sum = sum + x;
+		x = c;
+		if(count % 2 == 0)
+		{
+			long int z = 0;
+			z += sum;
+		}
+	}
+	printf("%ln", z);
+	printf("\n");
+}
