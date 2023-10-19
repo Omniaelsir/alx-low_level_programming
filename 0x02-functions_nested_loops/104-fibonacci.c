ee4925bdc7a03ee1259373771b29bc2515cdf77b
omniaelsir <omniatagelsir1@gmail.com>
2023-10-18 18:48:56 -0700

    0x02

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,21 +6,21 @@
  * Return: return 0 on success
 */
 
-int main(void)
+int main (void)
 {
 	int count;
-	long int c;
-	long int sum = 2;
-	long int x = 1;
+	unsigned long c;
+	unsigned long sum = 2;
+	unsigned long x = 1;
 
 	printf("1, 2");
-	for(count = 2; count <= 98; count++)
+	for (count = 2; count <= 98; count++)
 	{
 		c = sum;
 		sum = sum + x;
 		x = c;
 		printf(", ");
-		printf("%li" , sum);
+		printf("%lu", sum);
 	}
 	printf("\n");
 	return(0);
