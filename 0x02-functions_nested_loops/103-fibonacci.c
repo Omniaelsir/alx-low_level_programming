fb21d3fe419597985a7f23f1e18f16e6de966e60
omniaelsir <omniatagelsir1@gmail.com>
2023-10-18 18:37:13 -0700

    0x02

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,13 +8,15 @@
  * Return: return 0 on success
 */
 
-int main(void)
+int main (void)
 {
 	int count;
 	long int sum = 2;
 	long int x = 1;
 	long int c;
 	long int z = 2;
+
+	printf("1, 2");
 
 	for (count = 1; count <= 32; count++)
 	{
