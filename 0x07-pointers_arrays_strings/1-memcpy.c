35a896a0ffb25d63907f69bd227cb09d1d699f8e
Omniaelsir <omniatagelsir1@gmail.com>
2023-10-23 18:23:11 -0700

    0x07

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -20,7 +20,6 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	{
 		dest[x] = src[x];
 	}
-
 	return (dest);
 
 }
---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *_strchr - function that locates a character in a string
+ *
+ * @s: function parameter
+ *
+ * @c: function parameter
+ *
+ * Return: returns a pointer to s
+*/
+
+char *_strchr(char *s, char c)
+{
+	int itt;
+
+	for (itt = 0; s[itt] >= '\0'; itt++)
+	{
+		if (s[itt == c])
+		{
+			return (s + itt);
+		}
+	}
+	return ('\0');
+}
