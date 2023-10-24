ad4695915f94d4748393459fb6abab733613787d
Omniaelsir <omniatagelsir1@gmail.com>
2023-10-23 17:54:28 -0700

    0x07

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * *_memcpy - copies memory area
+ *
+ * @dest: function parameter
+ *
+ * @src: function parameter
+ *
+ * @n: function parameter/number of bytes
+ *
+ * Return: returns dest
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n);
+{
+	unsigned int x;
+
+	for (x = 0, x < n; x++)
+	{
+		dest[x] = src[x];
+	}
+
+	return (dest);
+
+}
