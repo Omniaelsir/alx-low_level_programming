ee87b7dab747628346327d80e1c526c50124d3cb
Omniaelsir <omniatagelsir1@gmail.com>
2023-10-25 16:25:34 -0700

    0x08

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_strlen_recursion - returns string's length
+ *
+ *@s: function parameter
+ *
+ *Return: length
+*/
+
+int _strlen_recursion(char *s)
+{
+	int count;
+	count = 0;
+
+	if (*s > '\0')
+
+		count += _strlen_recursion(s + 1) + 1;
+	return (count);
+}
