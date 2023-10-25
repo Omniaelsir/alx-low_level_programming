8976a68f07099fb3c5172e08c6a7b16ece654ada
Omniaelsir <omniatagelsir1@gmail.com>
2023-10-25 15:49:24 -0700

    0x08

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,12 +8,12 @@
 
 void _puts_recursion(char *s)
 {
-	if (s = '\0')
+	if (*s == '\0')
 		return;
 
 	else
 	{
-		_putchar('s');
-		_puts_recursion(s - 1);
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
 }
