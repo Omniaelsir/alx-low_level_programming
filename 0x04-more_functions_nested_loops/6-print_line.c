7fa5471c4486c3b200def3da88e31bddad882d73
Omniaelsir <omniatagelsir1@gmail.com>
2023-10-26 11:35:01 -0700

    0x04

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_line - draws straight line
+ *
+ * @n: function parameter
+ *
+ * Returns: void
+*/
+
+void print_line(int n)
+{
+	int count;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (count = 0; count <= n; count++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
