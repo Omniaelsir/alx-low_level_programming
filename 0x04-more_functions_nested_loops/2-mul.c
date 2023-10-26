2c4b7114530a730ecd46a1a43b03a50337fef127
Omniaelsir <omniatagelsir1@gmail.com>
2023-10-26 06:11:16 -0700

    “0x04”

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,3 +1,18 @@
 #include "main.h"
 
+/**
+ * mul - multiplies two integers
+ *
+ * @a: function parameter
+ *
+ * @b: function parameter
+ *
+ * Return: intergers multiplication
+*/
 
+int mul(int a, int b)
+{
+	return (a * b);
+
+	_putchar('\n');
+}
