421760084ddd3b478ef19ca66191664b4d7f4764
Omniaelsir <omniatagelsir1@gmail.com>
2023-10-25 17:13:14 -0700

    0x08

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * _pow_recursion - returns x to power y
+ *
+ * @x: function parameter
+ *
+ * @y: function paramter
+ *
+ * Return: returns x to power y
+*/
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1)
+
+	x *= _pow_recursion(x, y - 1);
+	return (x);
+
+}
