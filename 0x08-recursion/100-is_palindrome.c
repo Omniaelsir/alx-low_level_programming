ae7e284d17cf3f63975d34489b70d92a6b0a0b37
Omniaelsir <omniatagelsir1@gmail.com>
2023-10-25 18:40:46 -0700

    0x08

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * is_palindrome - is the string palindrome or not
+ *
+ * @s: string/ function parameter
+ *
+ * Return: 1 if string is palindrome
+ * 		0 if string is not palindrome
+*/
+
+int is_palindrome(char *s);
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * is_prime_number - is n a prime or not prime number
+ *
+ * @n: function paraeter
+ *
+ * Return: 1 if prime, 0 if not prime
+*/
+/**
+ * pass - function to check if number is a multiple of n
+ *
+ * @x: function parameter
+ *
+ * Return: */
+
+int pass(int n, int x);
+
+int is_prime_number(int n)
+{
+	return(pass(n, 2));
+}
+
+int pass(int n, int x)
+{
+	if (n % x == 0 || n <= 1)
+		return (0);
+
+	else if (x >= n && n > 1)
+		return (1);
+
+	else
+		return (pass(n, x +1));
+
+}
