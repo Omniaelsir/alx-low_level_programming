f8ff9fb37c0409b39c339cd0376a482fb42e8228
Omniaelsir <omniatagelsir1@gmail.com>
2023-10-26 06:42:11 -0700

    “0x04”

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,17 +3,18 @@
 /**
  * more_numbers - print 0 to 14, 10 times
  *
- * Return: void*/
+ * Return: void
+*/
 
 void more_numbers(void)
 {
-	for (int n = 0; n <= 9; n++)
+for (int n = 0; n <= 9; n++)
+{
+	for (int b = 1; b <= 14; b++)
 	{
-		for (int b = 1; b <= 14; b++)
-		{
-			_putchar(b + 48);
-		}
-		_putchar('\n');
+		_putchar(b + 48);
 	}
 	_putchar('\n');
+}
+_putchar('\n');
 }
