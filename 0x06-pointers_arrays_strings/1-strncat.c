8ec18e969c4a15097dc37897d71b937fda1a1193
Omniaelsir <omniatagelsir1@gmail.com>
2023-10-27 16:38:23 -0700

    0x06

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings
+ *
+ * @dest: string function parameter
+ *
+ * @src: string function parameter
+ *
+ * @n: int function parameter
+ *
+ * Return: string*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int X = 0;
+	int Y;
+
+	while (dest[X])
+	{
+		X++;
+	}
+	for (Y = 0; Y < n && (src[Y] != '\0'); Y++)
+	{
+		dest[X + Y] = src[Y];
+	}
+	dest[X + Y] = '\0';
+
+	return (dest);
+}
