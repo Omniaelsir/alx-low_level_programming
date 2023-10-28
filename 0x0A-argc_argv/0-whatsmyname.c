b5a49b5307ab6c0edfa159b8e2d7a9a033d1d921
Omniaelsir <omniatagelsir1@gmail.com>
2023-10-27 18:36:21 -0700

    0x0A

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry to program
+ * @argc: main parameter
+ * @argv: main parameter
+ * Return: returns 0
+*/
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s", argv[0]);
+	printf("\n");
+	return (0);
+}
